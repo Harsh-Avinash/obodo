1b13a27fe3299b5cb92a0fe61b170bf10502d7a6
Siddharth S Singh <simsid66@gmail.com>
2021-03-21 09:39:08 +0530

    Finish registration form

--- client_src/main.js
+++ client_src/main.js
@@ -7,13 +7,15 @@ const LoginPage = {
 
 function onGeolocateSuccess(location) {
     LoginPage.currentLocation = location.coords;
+    document.getElementById('submit-button').disabled = false;
 }
 
 function onGeolocateError(error) {
     let errorStr = '';
+    // TODO: replace with DOM alerts
     switch (error.code) {
         case error.PERMISSION_DENIED:
-            errorStr = "Please allow access to your location to continue. Reload the page to see the dialog again."
+            errorStr = "Please allow access to your location to continue. Click the icon in the left of your address bar to change the location permission."
             break;
         case error.POSITION_UNAVAILABLE:
             errorStr = "Location information is unavailable."
@@ -25,30 +27,46 @@ function onGeolocateError(error) {
             errorStr = "An unknown error occurred."
             break;
     }
-    alert(errorStr);
-    window.location.reload();
+    alert("Registration failed: " + errorStr);
 }
 
 
 function init() {
     let regBtn = document.getElementById('register');
     regBtn.addEventListener('click', function() {
-        let form = document.getElementById('login-form');
+        this.innerHTML = 'login';
+        let form = document.getElementById('initial-login');
         let header = form.getElementsByTagName('h1')[0];
         let submitBtn = document.getElementById('submit-button');
         if (LoginPage.currentPageState = 'login') {
             header.innerHTML = 'Register';
             LoginPage.currentPageState = 'regn';
             submitBtn.disabled = true;
+            submitBtn.value = 'Register';
+            form.action = 'register';
             if (window.navigator && window.navigator.geolocation) {
+                // TODO: DOM alert
                 alert("We need your location to register you successfully. Click Allow when prompted.");
                 navigator.geolocation.getCurrentPosition(onGeolocateSuccess, onGeolocateError);
             }
+            for (let x of form.childNodes) {
+                if (x.type != 'email' && x.type != 'password') {
+                    form.removeChild(x);
+                }
+            }
+            createField(form, 'Name', true)
+            createField(form, 'PhoneNo', false);
+            createField(form, 'Address', false);
+            createField(form, 'Coordinates', false);
+            document.getElementById('CoordinatesField').style.display = 'none';
+            CoordinatesField.innerHTML = LoginPage.currentLocation;
+
         }
         else {
+            this.innerHTML = 'register';
             header.innerHTML = 'Login';
             LoginPage.currentPageState = 'login';
-
+            form.action = 'auth';
             for (let x of form.childNodes) {
                 if (x.type != 'email' && x.type != 'password') {
                     form.removeChild(x);
@@ -57,8 +75,15 @@ function init() {
         }
     });
 
-    function createField() {
+    function createField(form, value, reqd) {
         let elem = document.createElement('input');
         elem.type = 'text';
+        elem.id = value + 'Field';
+        elem.name = value;
+        elem.required = reqd;
+        elem.placeholder = `your ${value}`
+        let submitBtn = document.getElementById('submit-button');
+        form.insertBefore(elem, submitBtn);
+        return elem;
     }
 }
--- main.js
+++ main.js
@@ -29,24 +29,53 @@ app.get('/', function (request, response) {
 app.post('/auth', function (request, response) {
     let email = request.body.email;
     let password = request.body.password;
-    response.send(email);
+    response.send(`Attempting to log you in as ${email}...`);
     
     if (email && password) {
         let query = `SELECT email, name from users where email = ?`;
         let params = [email];
         let existingEmail = db.get(query, params, function(error, row) {
-
+            if (!error) {
+                response.send('Found email.');
+            }
+            else {
+                response.send(`Error: no account with email ${email} found.`);
+            }
         })
-        console.log(existingEmail)
-/*         bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
-            // Store hash in your password DB.
-        }); */
+        console.log(existingEmail);
         response.end();
     }   
 });
 
 app.post('/register', function (request, response) {
-
+    bcrypt.hash(request.password, SALT_ROUNDS, function (err, hash) {
+        // Store hash in your password DB.
+        console.log(hash);
+        if (err) {
+            response.send('Error creating account');
+        }
+        else {
+            console.log('valid')
+            if (!request.PhoneNo) {
+                request.PhoneNo = "NULL";
+            }
+            if (!request.Address) {
+                request.Address = "NULL";
+            }
+            let params = [request.email, hash, request.name, request.PhoneNo, request.Coordinates, request.Address];
+            db.run('INSERT INTO users(email, password, name, phone, coordinates, address) values(?)', params, function (err) {
+                if (err) {
+                    response.send('Error creating account.');
+                    return console.log(err.message);
+                    
+                }
+                // get the last insert id
+                response.send('Account created successfully');
+                console.log(`A row has been inserted with rowid ${this.lastID}`);
+            });
+        }
+    });
+    response.end();
 })
 
 app.listen(8080);
